fd180c245d5e53fd9e31aca74eb0fdbe665a07ed
Familjen <malo@Strabys-iMac.local>
2014-04-20 15:42:30 +0800

    Initial Commit

---
+++ GitTest/BOSDocument.h
@@ -0,0 +1,13 @@
+//
+//  BOSDocument.h
+//  GitTest
+//
+//  Created by Familjen on 2014-04-20.
+//  Copyright (c) 2014 OAY. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BOSDocument : NSDocument
+
+@end
